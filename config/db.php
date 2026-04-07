7a1e767dfaaea56e210937a3b3de85a9d42c543a
Untilmike <mikewachira02@gmail.com>
2026-04-07 08:57:20 +0300

    Update database config for hosting

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'card_vault');
+define('DB_HOST', 'sql311.infinityfree.com');  // MySQL Host Name
+define('DB_USER', 'if0_41594028');           // MySQL User Name
+define('DB_PASS', 'Ruiru240');   // MySQL Password
+define('DB_NAME', 'if0_41594028_epiz_123456_cardvault'); // MySQL DB Name
 define('AES_KEY', 'ExactlySixteen12');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
